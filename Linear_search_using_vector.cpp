0e75e7b7d125449df26c6348f6fc340f447a3c6f
shou-1202 <shouryagarg1202@gmail.com>
2025-09-28 10:42:18 +0530

    first commit

---
+++ Linear_search_using_vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector <int> nums(7);
+    for(int val: nums){
+        cout<<"Enter value:\t";
+        cin>>val;
+    }
+    int value;
+    
+}
